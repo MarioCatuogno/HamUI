9a0fc5503bda6eadfeec88d79561ab48bdac0336
Mario Catuogno <mario.catuogno@gmail.com>
2022-12-19 12:10:45 +0100

    Cleanup .lua script

--- Interface/AddOns/HamUI/core.lua
+++ Interface/AddOns/HamUI/core.lua
@@ -101,25 +101,6 @@ TargetFrame.maxDebuffs = 5;
 TargetFrame.TargetFrameContent.TargetFrameContentMain.ReputationColor:SetTexture(nil)
 FocusFrame.TargetFrameContent.TargetFrameContentMain.ReputationColor:SetTexture(nil)
 
----- Hide resource bar under Player Frame
---local ResourceBarPosition = CreateFrame("Frame")
---    ResourceBarPosition:RegisterEvent("PLAYER_LOGIN")
---    ResourceBarPosition:RegisterEvent("PLAYER_ENTERING_WORLD")
---
---    ResourceBarPosition:SetScript("OnEvent", function(self, event, ...)
---
---        -- Monk Chi points bar
---        MonkHarmonyBarFrame:ClearAllPoints()
---        MonkHarmonyBarFrame:SetScale(1.0)
---        MonkHarmonyBarFrame:SetPoint("CENTER",UIParent,"CENTER", 0, -190)
---
---        -- Monk Stagger bar
---        MonkStaggerBar:ClearAllPoints()
---        MonkStaggerBar:SetScale(1.1)
---        MonkStaggerBar:SetPoint("CENTER",UIParent,"CENTER", 0, -170)
---
---end)
-
 -- Hide Reputation/XP bar
 StatusTrackingBarManager:UnregisterAllEvents()
 StatusTrackingBarManager:SetScript("OnShow", function() StatusTrackingBarManager:Hide() end)
